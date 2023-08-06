c24c1576a19344d0515144f7903d654916bdfcf3
Arif <140314705+arif-bcse@users.noreply.github.com>
2023-08-06 14:24:05 +0600

    Add files via upload

---
+++ Lab 01/71a.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the weight of the Berry :\n";
+    cin>>n;
+    if(n%2==0)
+        cout<<"YES\n";
+    else
+        cout<<"NO\n";
+    return 0;
+}
