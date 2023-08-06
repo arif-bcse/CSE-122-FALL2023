f7d7eee1b98877e175cc552b72a456641681e606
Arif <140314705+arif-bcse@users.noreply.github.com>
2023-08-06 12:16:44 +0600

    Add files via upload

---
+++ w.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x;
+    cout<<"Enter the weight of the Berry  : ";
+    cin>>x;
+    if(x%2==0)
+        cout<<"YES";
+        else
+        cout<<"NO";
+    return 0;
+}
